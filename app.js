1b3fd6c09baac42cc744a40435408c5ed3a57fa8
Soubhagya Ranjan Sethi <93478954+soubhagyagithub@users.noreply.github.com>
2024-12-31 22:18:44 +0530

    type inference and type casting

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+var num1Element = document.getElementById("num1");
+var num2Element = document.getElementById("num2");
+var btnElement = document.querySelector("button");
 function add(num1, num2) {
     return num1 + num2;
 }
-console.log(add(1, 6));
-console.log(add('1', '6'));
+btnElement.addEventListener('click', function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    console.log(add(+num1, +num2));
+});
--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
-function add(num1,num2){
-    return num1 + num2
+const num1Element = document.getElementById("num1") as HTMLInputElement
+const num2Element = document.getElementById("num2") as HTMLInputElement
+const btnElement = document.querySelector("button")
+function add(num1: number, num2: number) {
+    return num1 + num2;
 }
+btnElement.addEventListener('click',()=>{
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+    console.log(add(+num1,+num2))
 
-console.log(add(1,6))
-console.log(add('1','6'))
+})
+
+;
