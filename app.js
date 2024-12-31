15f053495b270d0aad3c1b5186b825bbcaf92b1e
Soubhagya Ranjan Sethi <93478954+soubhagyagithub@users.noreply.github.com>
2024-12-31 23:24:35 +0530

    Object and Array Types

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var num1Element = document.getElementById("num1");
 var num2Element = document.getElementById("num2");
 var btnElement = document.querySelector("button");
+var numResult = [];
+var stringResult = [];
 function add(num1, num2) {
     if (typeof num1 === 'number' && typeof num2 === 'number') {
         return num1 + num2;
@@ -10,11 +12,16 @@ function add(num1, num2) {
     }
     return +num1 + +num2;
 }
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
 btnElement.addEventListener('click', function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
     var result = add(+num1, +num2);
+    numResult.push(result);
     var resultString = add(num1, num2);
-    console.log(result);
-    console.log(resultString);
+    stringResult.push(resultString);
+    printResult({ val: result, timestamp: new Date() });
+    console.log(numResult, stringResult);
 });
--- app.ts
+++ app.ts
@@ -2,6 +2,9 @@ const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const btnElement = document.querySelector("button")!;
 
+const numResult: number[] = [];
+const stringResult: string[] = [];
+
 function add(num1: number | string, num2: number | string) {
     if(typeof num1 === 'number' && typeof num2 === 'number'){
         return num1 + num2
@@ -11,14 +14,21 @@ function add(num1: number | string, num2: number | string) {
     return +num1 + +num2;
 }
 
+function printResult(resultObj: {val: number, timestamp: Date}){
+    console.log(resultObj.val);
+    
+}
+
 
     btnElement.addEventListener('click',()=>{
         const num1 = num1Element.value;
         const num2 = num2Element.value;
         const result  = add(+num1,+num2);
+        numResult.push(result as number);
         const resultString  = add(num1,num2);
-        console.log(result);
-        console.log(resultString);
+        stringResult.push(resultString as string);
+        printResult({val: result as number, timestamp: new Date()});
+        console.log(numResult, stringResult);
         
     
     })
