8d8539db098b2c3939b2cd7ed89e5ca4f64de916
Soubhagya Ranjan Sethi <93478954+soubhagyagithub@users.noreply.github.com>
2024-12-31 23:01:44 +0530

    Union Types

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ var num1Element = document.getElementById("num1");
 var num2Element = document.getElementById("num2");
 var btnElement = document.querySelector("button");
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
 }
 btnElement.addEventListener('click', function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
-    console.log(add(+num1, +num2));
+    var result = add(+num1, +num2);
+    var resultString = add(num1, num2);
+    console.log(result);
+    console.log(resultString);
 });
--- app.ts
+++ app.ts
@@ -1,14 +1,26 @@
-const num1Element = document.getElementById("num1") as HTMLInputElement
-const num2Element = document.getElementById("num2") as HTMLInputElement
-const btnElement = document.querySelector("button")
-function add(num1: number, num2: number) {
-    return num1 + num2;
+const num1Element = document.getElementById("num1") as HTMLInputElement;
+const num2Element = document.getElementById("num2") as HTMLInputElement;
+const btnElement = document.querySelector("button")!;
+
+function add(num1: number | string, num2: number | string) {
+    if(typeof num1 === 'number' && typeof num2 === 'number'){
+        return num1 + num2
+    }else if(typeof num1 === 'string' && typeof num2 === 'string'){
+        return num1+ ' '+ num2
+    }
+    return +num1 + +num2;
 }
-btnElement.addEventListener('click',()=>{
-    const num1 = num1Element.value;
-    const num2 = num2Element.value;
-    console.log(add(+num1,+num2))
 
-})
 
-;
+    btnElement.addEventListener('click',()=>{
+        const num1 = num1Element.value;
+        const num2 = num2Element.value;
+        const result  = add(+num1,+num2);
+        const resultString  = add(num1,num2);
+        console.log(result);
+        console.log(resultString);
+        
+    
+    })
+
+
